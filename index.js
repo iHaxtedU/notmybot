e4bded5c4a22c94878e940ef2df1c3d3ee37a008
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-04-29 22:15:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,26 @@ bot.on("message", async message => {
     message.channel.send("Pong");
 
   }
+  if(cmd === `${prefix}contact`){
+   let iUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!iUser) return message.channel.send("Can't Find User!");
+  let iReason = args.join(" ").slice(22);
+  let msg = ("You Were Called To Encry For...");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You Do Not Have This Permission");
+
+  let contactEmbed = new Discord.RichEmbed()
+  .setDescription("~You Have Been Contacted~")
+  .setColor("#1eff3c")
+  .addField("You Have Been Contacted For", iReason)
+  .addField("Sent By", `<@${message.author.id}> with ID ${message.author.id}`);
+
+try{
+    await iUser.send(contactEmbed)
+  message.channel.send("Message Successfully Sent")
+  }catch(e){
+    message.channel.send("You Message Could Not Be Sent Because The User Has DM's Disabled")
+  }
+}
 
   });
 
