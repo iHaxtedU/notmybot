8aef590efc76965e99df942ac3edaeacd1268088
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-04-30 15:57:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-  let coins = require("./Tokens.json");
 
   
 if(cmd === `${prefix}tokens`){
