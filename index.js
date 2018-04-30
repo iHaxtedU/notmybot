282bb5d7bfbbe32d7a609fd7918246072a3a7135
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-04-30 16:01:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
+  if(!coins[message.author.id]){
+  coins[message.author.id] = {
+    coins: 0
+  };
+}
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
