121a34ded6be0c56a5c271cfa3554db90553befc
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-02 16:46:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.on("message", async message => {
   let User = message.mentions.users.first();
   if(!User) return message.channel.send("Can't Find User!");
   let ammount = args.join(" ").slice(22);
-  if (!ammount) isNan() return message.channel.send("Please use a number");
+  if (!ammount.isNaN()) return message.channel.send("Please use a number");
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You Do Not Have This Permission");
     
     
