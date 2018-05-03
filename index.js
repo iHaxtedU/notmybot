1acefc1dbc4fa86c1e0f05406a62a00c99eba40a
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-02 21:35:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ let uCoins = coins[cuser.id].coins;
 let coinEmbed = new Discord.RichEmbed()
 .setAuthor(message.author.username)
 .setColor("00FF00")
-.addField("Here's The Information You Requested, The User Has", `${uCoins} coins!}`);
+.addField("Here's The Information You Requested.", `The User Has ${uCoins} Coins!`);
 
 message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
 }
