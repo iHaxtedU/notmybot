5290444a1a0676f0931b1f7c623a4ed980078f16
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-02 19:17:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,25 @@ bot.on("message", async message => {
 
   if(cmd === `${prefix}pingme`){
     message.channel.send("Pong");
+    }
+  
+  if(cmd === `${prefix}tokens`){
+    let coins = require("./Tokens.json")
+if(!coins[User.id]){
+  coins[User.id] = {
+    coins: 0
+  }};
 }
+
+let uCoins = coins[message.author.id].coins;
+
+let coinEmbed = new Discord.RichEmbed()
+.setAuthor(message.author.username)
+.setColor("00FF00")
+.addField("You Have", uCoins);
+
+message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+  }
      if(cmd === `${prefix}givetokens`){
   let coins = require("./Tokens.json")
   let User = message.mentions.users.first();
@@ -58,23 +76,6 @@ fs.writeFile("./Tokens.json", JSON.stringify(coins), (err) => {
 if (err) console.log(err)
 });
 }
-  
-if(cmd === `${prefix}tokens`){
-if(!coins[User.id]){
-  coins[User.id] = {
-    coins: 0
-  }};
-}
-
-let uCoins = coins[message.author.id].coins;
-
-let coinEmbed = new Discord.RichEmbed()
-.setAuthor(message.author.username)
-.setColor("00FF00")
-.addField("You Have", uCoins);
-
-message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
-  }
     
   if(cmd === `${prefix}contact`){
   let iUser = message.guild.member(message.mentions.users.first());
