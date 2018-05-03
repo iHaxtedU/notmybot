9809ca1c6b361594a61f31ab2cc7cf51d512354a
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-02 19:22:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,24 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
+  let coins = require("./Tokens.json");
+  
+  if(cmd === `${prefix}tokens`){
+    if(!coins[message.author.id]){
+  coins[message.author.id] = {
+    coins: 0
+  };
+}
+
+let uCoins = coins[message.author.id].coins;
+
+let coinEmbed = new Discord.RichEmbed()
+.setAuthor(message.author.username)
+.setColor("00FF00")
+.addField("You Have", uCoins)
+
+message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+}
 
   if(cmd === `${prefix}pingme`){
     message.channel.send("Pong");
