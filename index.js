db6a32481e9c5830c14f32f34ec7557ab5333691
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-02 21:26:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ bot.on("message", async message => {
 
 let cuser = message.mentions.member.first();
 let uCoins = coins[cuser.id].coins;
-if(!uCoins) let uCoins = coins[message.author.id].coins;
+if(!uCoins) coins[message.author.id].coins;
 
 let coinEmbed = new Discord.RichEmbed()
 .setAuthor(message.author.username)
