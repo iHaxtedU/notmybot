f6ca5771fca1f2d6245840547c1b22e84e0d873c
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-16 08:02:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
-let coins = require("./coins.json");
+let coins = require("./Tokens.json");
 
 fs.readdir("./commands/", (err, files) => {
 
