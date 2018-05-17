dd84d6f7559206146dc7af695416ae84e34dc3d1
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-16 20:52:35 -0500

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+message.channel.send(`Pong! \`${bot.pings[0]}ms\``);
+}
+
+module.exports.help = {
+  name: "ping"
+}
